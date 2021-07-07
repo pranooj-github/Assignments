b0364eee9a9f9dafd1c16b907b815207ea5ddac1
pranoojpv <pranooj.poovathan@gmail.com>
2021-07-08 00:08:38 +0530

    commiting assignment 2

---
+++ JAVA/Assignment2/program_start.java
@@ -0,0 +1,60 @@
+class employee {
+
+
+    public void display_salary()
+    {
+      System.out.println("SALARY INFORMATION");
+      System.out.println("*******************");
+    }
+}
+
+
+class manager extends employee {
+
+    int pf,tax,incentive,baseSalaryOfManager;
+          manager( int baseSalaryOfManager, int pf, int tax,int incentive) {
+          this.baseSalaryOfManager=baseSalaryOfManager;
+          this.pf=pf;
+          this.tax=tax;
+          this.incentive=incentive;
+    }
+
+
+
+    public void display_salary() {
+      int netSalary=baseSalaryOfManager-(pf+tax)+ incentive;
+
+        System.out.println("NetSalary of this Manager is:"+netSalary+"rs");
+    }
+}
+
+ class labour extends employee {
+      int baseSalaryOfLabour,pf,tax,overtime;
+      labour(int pf,int tax,int overtime,int baseSalaryOfLabour) {
+      this.baseSalaryOfLabour=baseSalaryOfLabour;
+      this.pf=pf;
+      this.tax=tax;
+      this.overtime=overtime;
+    }
+
+
+    public void display_salary() {
+      int netSalary=baseSalaryOfLabour-(pf+tax)+ overtime;
+
+        System.out.println("NetSalary of this Labour is: "+netSalary+"rs");
+    }
+}
+public class program_start {
+    public static void main(String[] args) {
+
+      employee em=new employee();
+      em.display_salary();
+        manager man=new manager(50000,1000,200,1000);
+        man.display_salary();
+        labour lab = new labour(30000,1000,200,100);
+        lab.display_salary();
+
+
+
+    }
+}
